228cb0dfa41917ec1c594ac9265f9356ad8d2868
amanuel <amanuelisaak15@gmail.com>
2024-01-22 17:40:41 +0100

    commit, i finished the random generator

---
+++ password.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+  "fmt"
+)
+
+const (
+	letterBytes  = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	numBytes     = "0123456789"
+	specialBytes = "!@#$%^&*()_+{}[]|;:,.<>?/~`"
+)
+
+var yesOrNo string
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func PasswordGenerator(length int, useNum, useSpecial, useLetter bool) string {
+	var allowedChars string
+	if useLetter {
+		allowedChars += letterBytes
+	}
+	if useNum {
+		allowedChars += numBytes
+	}
+	if useSpecial {
+		allowedChars += specialBytes
+	}
+
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = allowedChars[rand.Intn(len(allowedChars))]
+	}
+	return string(b)
+}
+
+func main() {
+
+  var length int
+
+  fmt.Println("wanna password (y/N): ")
+  fmt.Scanln(&yesOrNo)
+
+  if yesOrNo == "y" {
+    fmt.Println("the length? ", )
+    fmt.Scanln(&length)
+    
+    if length < 4 || length > 25 {
+      fmt.Println("invalid, short or too long")
+    } else {
+  password := PasswordGenerator(length, true, true, true)
+      fmt.Println("passowrd: ", password)
+    }
+  } else {
+    fmt.Println("bye!!!")
+  }
+}
